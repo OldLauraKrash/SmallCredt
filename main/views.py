a35d5ef30cfd90e0a71127a1057f1a9bd932ff86
Dima Kalinin <kal1sha.ua@gmail.com>
2013-04-25 12:17:57 +0300

    Fixed login

--- main/views.py
+++ main/views.py
@@ -5,6 +5,7 @@ from client.models import *
 import hashlib
 import simplejson as json
 from django.http import HttpResponseRedirect
+from django.db.models import Q
 
 @render_to('main/index.html')
 def home(request):
@@ -14,7 +15,7 @@ def login(request):
 	result = 'error'
 	hsh = hashlib.md5()
 	hsh.update(request.GET['password'])
-	client = Client.objects.filter(email=request.GET['email'], password=hsh.hexdigest())	
+	client = Client.objects.get(email__exact=request.GET['email'], password__exact=hsh.hexdigest())
 	try: 
 		if client.email!='':
 			request.session['username'] = client.email
